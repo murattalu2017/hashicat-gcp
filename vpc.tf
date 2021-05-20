94120de986ee14e6ef87495b69ed10702a43c6cd
murattalu2017 <murattalu2017@gmail.com>
2021-05-20 17:36:48 +0000

    Added the first billable

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/murattalu2021-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = "db_net"
+  network_name = "testi-net"
   project_id = var.project
   subnets = [
   {
