d6fcb77f27a8edf2f0670f2d9d7e7b5b5b38a0f6
murattalu2017 <murattalu2017@gmail.com>
2021-05-20 17:34:13 +0000

    Added the first billable

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "network" {
   version = "2.5.0"
   # insert required variables here
   network_name = "db_net"
-  project_id = var.project_id
+  project_id = var.project
   subnets = [
   {
     subnet_name   = "gram-subnet"
